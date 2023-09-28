b59eafd505de8be543a3ba605cd8260364c03726
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-09-28 11:01:21 +0530

    add 23

--- app/(secure)/search/page.tsx
+++ app/(secure)/search/page.tsx
@@ -52,8 +52,7 @@ function Edit() {
       </div>
 
       <div className="container  p-0">
-
-        {/* {loan?.users?.length > 0 ? (
+{loan?.user? loan?.users?.length > 0 ? (
           <>
             <div className=" ">
               <div className="flex flex-col md:flex-row w-full  space-x-1 ">
@@ -164,7 +163,8 @@ function Edit() {
           </>
         ) : (
           ""
-        )} */}
+        ):""}
+        {}
       </div>
     </div>
         </> )
