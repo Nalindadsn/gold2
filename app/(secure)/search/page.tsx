524f658d2a526ddcf4d72f902fc8734a76a2e599
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-09-28 12:07:35 +0530

    add 2345

--- app/(secure)/search/page.tsx
+++ app/(secure)/search/page.tsx
@@ -62,46 +62,47 @@ function Edit() {
                   </h3>
                   <div className="border">
                     {loan?.users[0]?.my_loans?.map((i: any) => (
-                        <div  key={i?._id} className="my-1 px-1 w-full ">
-                          <article className="overflow-hidden rounded-lg shadow-md border">
-                            <header className="flex items-center justify-between leading-tight pb-0 p-2 md:p-4">
-                              <h1 className="text-lg">
-                              <div
-                                className="no-underline hover:underline text-black text-sm ml-2"
-                              >
-                                Ref No : {i?._id}
-                              </div>
-                              <div
-                                className="no-underline hover:underline text-black text-sm ml-2"
-                              >
-                                Form Number : {i?.form_number}
-                              </div>
-                              </h1>
-                              <p className="text-grey-darker text-sm">
-                                {i?.createdAt}
-                              </p>
-                            </header>
+                      <></>
+                        // <div  key={i?._id} className="my-1 px-1 w-full ">
+                        //   <article className="overflow-hidden rounded-lg shadow-md border">
+                        //     <header className="flex items-center justify-between leading-tight pb-0 p-2 md:p-4">
+                        //       <h1 className="text-lg">
+                        //       <div
+                        //         className="no-underline hover:underline text-black text-sm ml-2"
+                        //       >
+                        //         Ref No : {i?._id}
+                        //       </div>
+                        //       <div
+                        //         className="no-underline hover:underline text-black text-sm ml-2"
+                        //       >
+                        //         Form Number : {i?.form_number}
+                        //       </div>
+                        //       </h1>
+                        //       <p className="text-grey-darker text-sm">
+                        //         {i?.createdAt}
+                        //       </p>
+                        //     </header>
 
-                            <footer className="flex items-center justify-between leading-none p-2 pt-1 md:p-4">
-                              <a
-                                className="flex items-center no-underline hover:underline text-black"
-                                href="#"
-                              >
-                                <p className="ml-2 text-sm">
-                                  Loan LKR{" "}
-                                  {parseFloat(i?.loan_amount).toFixed(2)} for{" "}
-                                  {i?.no_of_month} month
-                                </p>
-                              </a>
-                              <a
-                                className="no-underline text-grey-darker hover:text-red-dark"
-                                href="#"
-                              >
-                                <span>{i?.status}</span>
-                              </a>
-                            </footer>
-                          </article>
-                        </div>
+                        //     <footer className="flex items-center justify-between leading-none p-2 pt-1 md:p-4">
+                        //       <a
+                        //         className="flex items-center no-underline hover:underline text-black"
+                        //         href="#"
+                        //       >
+                        //         <p className="ml-2 text-sm">
+                        //           Loan LKR{" "}
+                        //           {parseFloat(i?.loan_amount).toFixed(2)} for{" "}
+                        //           {i?.no_of_month} month
+                        //         </p>
+                        //       </a>
+                        //       <a
+                        //         className="no-underline text-grey-darker hover:text-red-dark"
+                        //         href="#"
+                        //       >
+                        //         <span>{i?.status}</span>
+                        //       </a>
+                        //     </footer>
+                        //   </article>
+                        // </div>
                     ))}
                   </div>
                 </div>
