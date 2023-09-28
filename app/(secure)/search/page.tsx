18569e5f9e2cb5f1f17dfd4190ca8eb587e93d3e
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-09-28 15:48:26 +0530

    ad5new679

--- app/(secure)/search/page.tsx
+++ app/(secure)/search/page.tsx
@@ -25,7 +25,7 @@ function Edit() {
               <div className="m-2">
           
                   <div>Full Name : {loan?.users?loan?.users[0]?.fullName:"FULL NAME"}</div>
-                        <div>NIC : {loan?.users?loan?.users[0]?.nic:"000000000V"}</div>
+                        {/* <div>NIC : {loan?.users?loan?.users[0]?.nic:"000000000V"}</div> */}
                 
               </div>
             </div>
