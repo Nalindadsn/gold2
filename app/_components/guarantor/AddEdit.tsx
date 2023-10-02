35f5316edf0ae1bdc809fb751f53af65b154c265
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-10-02 10:41:14 +0530

    add data234

--- app/_components/guarantor/AddEdit.tsx
+++ app/_components/guarantor/AddEdit.tsx
@@ -146,11 +146,10 @@ setValue("fullName", n++, {
 
     return (
         <>
-<h1 className="py-1 px-3  mt-3 block text-base font-semibold sm:text-xl ">{title}</h1>
+<h1 className="py-1  mt-3 block text-base font-semibold sm:text-xl ">{title}</h1>
 
 
 <form onSubmit={handleSubmit2(onSubmitCheck)} className=' bg-white '>
-            <h1 className='font-bold m-1'>Basic Details</h1>
 
 
                 <div className='p-2'>
