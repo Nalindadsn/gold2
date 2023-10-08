66d3bfc8225e81f3897d1d13806fcc61dfbdc11b
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-10-08 20:51:30 +0530

    button

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -245,16 +245,16 @@ function Sidebar() {
                                     <input type="text"
                                     name="nic"
                                     onChange={(e)=>setSearch(e.target.value)}
-                                        className=" "
+                                        className=" rounded-md bg-gray-700 text-sm text-gray-300 pl-10 py-1.5 ml-5 "
                                         placeholder="Search..." />
-                                    {/* <span
+                                    <span
                                         className="h-4 w-4 absolute left-44 bottom-2 text-gray-400 transition-all duration-300 ease-in-out group-focus-within:left-8">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none"
                                             viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                         </svg>
-                                    </span> */}
+                                    </span>
                                 </div>
                             </form>
                             
