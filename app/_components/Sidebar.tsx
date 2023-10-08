5bb16ad9b4fd3c7578a2bb13163788849e46744c
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-10-08 18:57:13 +0530

    button

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -97,17 +97,7 @@ function Sidebar() {
        
                  <div className='group-hover:text-gray-900 dark:group-hover:text-white'>
 
-                 <li  className="m-0  border-0 border-1 border-t">
-                    <div  className="flex items-center  px-3 text-gray-300     dark:hover:bg-gray-700 group">
-                      
-                    <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-3 h-3 text-gray-500" >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
-          </svg>
-                       
-                       <span className="flex-1 ml-3 whitespace-nowrap text-gray-500 text-sm ">Loans</span>
-                    </div>
                  
-                 </li>
                  <li  className="ml-2 mt-0">
                     <a href="/loans" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
                       
@@ -117,6 +107,29 @@ function Sidebar() {
                        <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
                     </a>
                  </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-approved" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Approved</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-rejected" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Rejected</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-completed" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Completed</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+
+
                  </div>
 
                  <li  className="m-0">
@@ -288,6 +301,33 @@ function Sidebar() {
                        <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
                     </a>
                  </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-approved" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Approved</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-rejected" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Rejected</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+                 <li  className="ml-2 mt-0">
+                    <a href="/loans-completed" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
+                       -
+                       <span className="flex-1 ml-3 whitespace-nowrap ">Loans Completed</span>
+                       <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
+                    </a>
+                 </li>
+
+
+
+
+
+
                  </li>
                  <li>
                     <a href="/settings" className="flex items-center py-2 px-3 text-gray-300   hover:bg-gray-300 hover:text-gray-800 dark:hover:bg-gray-700 group">
