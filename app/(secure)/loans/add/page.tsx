58173decc628f97845016d269b300ee73e5aac21
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-10-28 11:35:08 +0530

    add

--- app/(secure)/loans/add/page.tsx
+++ app/(secure)/loans/add/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { AddEdit } from '_components/loans';
-import { useRateService } from '_services';
+import { useRateService,useUserService } from '_services';
 import { Spinner } from '_components';
 
 export default Add;
@@ -12,9 +12,16 @@ function Add() {
     const rate = rateService.rate;
 
 
+    const userService = useUserService();
+    const user: any = userService.currentUser;
+    useEffect(() => {
+  
+      userService.getCurrent();
+    }, []);
+
     useEffect(() => {
         rateService.getSelected()
     }, []);
-    return rate?<AddEdit title="ADD LOAN DETAILS" rate={rate}/>:
+    return rate?<AddEdit title="ADD LOAN DETAILS" rate={rate} user={user}/>:
     <Spinner />;
 }
